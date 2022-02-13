a58f7cc12b8cff8b7c3b72965780ea4d2de37983
workerKwon <dmdmdm1322@gmail.com>
2022-02-13 21:14:53 +0900

    view와 model를 담을 수 있는 ModelView 객체를 추가 - ModelView의 viewName에는 논리적 viewName을 넣는다. - ModelView의 model에는 반환할 데이터를 Map으로 담는다. - viewResolver를 만들어서 논리적 viewName을 물리적 경로로 바꾼다.

--- src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
 
@@ -17,5 +18,21 @@ public class MyView {
     public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewPath);
         requestDispatcher.forward(request, response);
+    }
+
+    // model = ("member", Member), ("members", List<Member>)
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        modelToRequestAttribute(model, request);
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewPath);
+        requestDispatcher.forward(request, response);
+    }
+
+    /**
+     *
+     * @param model
+     * @param request
+     */
+    private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+        model.forEach((key, value) -> request.setAttribute(key, value));
     }
 }
