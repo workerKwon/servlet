c584b126df1264959bbfc52602940ef6726cbfa5
workerKwon <dmdmdm1322@gmail.com>
2022-02-13 21:14:53 +0900

    view와 model를 담을 수 있는 ModelView 객체를 추가 - ModelView의 viewName에는 논리적 viewName을 넣는다. - ModelView의 model에는 반환할 데이터를 Map으로 담는다. - viewResolver를 만들어서 논리적 viewName을 물리적 경로로 바꾼다.

--- src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
 
@@ -17,5 +18,17 @@ public class MyView {
     public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewPath);
         requestDispatcher.forward(request, response);
+    }
+
+    // model = ("member", Member), ("members", List<Member>)
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        modelToRequestAttribute(model, request);
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewPath);
+        requestDispatcher.forward(request, response);
+    }
+
+    // JSP에서 사용하려면 setAttribue에 넣어줘야 사용할 수 있다.
+    private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+        model.forEach((key, value) -> request.setAttribute(key, value));
     }
 }
---
+++ src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberSaveControllerV3.java
@@ -0,0 +1,31 @@
+package hello.servlet.web.frontcontroller.v3.controller;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+
+import java.util.Map;
+
+public class MemberSaveControllerV3 implements ControllerV3 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    /**
+     * 서블릿 종속성을 없앨 수 있다. HttpServletResquest, Response 사용하지 않는다.
+     * @param paramMap
+     * @return
+     */
+    @Override
+    public ModelView process(Map<String, String> paramMap) {
+        String username = paramMap.get("username");
+        int age = Integer.parseInt(paramMap.get("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        ModelView mv = new ModelView("save-result");
+        mv.getModel().put("member", member);
+        return mv;
+    }
+}
